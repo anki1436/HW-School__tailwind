0ba711d984374987fd67a4dd1397f31f7517f0cd
anki1436 <ankitbisht.neosoft@gmail.com>
2022-05-31 18:48:51 +0530

    tailwind HW school

---
+++ js/slick.js
@@ -0,0 +1,75 @@
+$('.student-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows:true,
+    // autoplay:true,
+    // variableWidth:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });$('.testimonal-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows:false,
+    // autoplay:true,
+    // variableWidth:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
---
+++ js/tailwind.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgba(132, 30, 37, 1)',  
+          secondary: 'rgba(255, 186, 73,1)',
+          white: 'rgba(255, 255, 255,1)',
+          black: 'rgba(0, 0, 0,1)',
+          grey:'#C8C8C8',
+          dark:"#040404",
+          borderColor:"#BDBDBD",
+          pink:"#FF5959",
+          green:"#70CE88",
+          blue:"#5C96FF",
+          yellow:"#FFBA49",
+          lightGrey:"rgba(55, 71, 79, 0.65)",
+          textColor:"#37474F",
+          bgInfo:"#841e2515"
+
+
+        }
+      }
+    }
+  }
